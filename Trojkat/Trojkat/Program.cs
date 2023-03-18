d280b2042c0a897be06ea7f91b6219edead54f57
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-03-18 13:29:57 +0100

    Add files via upload

---
+++ Trojkat/Trojkat/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Trojkat
+{ 
+    class Trojkat
+    {
+        int a;
+        
+        double h;
+        public int bok
+        {
+            set
+            {
+                a = value;
+
+                h = a * System.Math.Sqrt(3) / 2;
+            }
+        }
+        public double pole()
+        {
+            return a * h / 2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trojkat tr;
+
+            tr = new Trojkat();
+
+            tr.bok = 3;
+
+            Console.WriteLine(tr.pole());
+
+            tr.bok = 5;
+
+            Console.WriteLine(tr.pole());
+
+            
+        }
+    }
+}
